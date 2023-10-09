5ca7c17c9a380c2a446dc66ef5a0ed8213d65420
Joseph T. McQuigg <j.t.mcquigg12@gmail.com>
2023-10-08 23:57:13 -0400

    Arch Loom
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

---
+++ Common/build.gradle.kts
@@ -0,0 +1,53 @@
+import com.matyrobbrt.gradle.pngoptimiser.task.OptimisePNGTask
+
+architectury {
+    common("forge", "fabric")
+    platformSetupLoomIde()
+}
+
+plugins {
+    id("com.matyrobbrt.pngoptimiser") version "0.2.0"
+}
+
+val minecraftVersion = project.properties["minecraft_version"] as String
+
+loom.accessWidenerPath.set(file("src/main/resources/byg.accesswidener"))
+
+sourceSets.main.get().resources.srcDir("src/main/generated/resources")
+
+dependencies {
+    // We depend on fabric loader here to use the fabric @Environment annotations and get the mixin dependencies
+    // Do NOT use other classes from fabric loader
+    modImplementation("net.fabricmc:fabric-loader:${project.properties["fabric_loader_version"]}")
+
+    compileOnly("org.ow2.asm:asm-tree:9.5")
+    compileOnly("software.bernie.geckolib:geckolib-forge-${minecraftVersion}:${project.properties["geckolib_version"]}")
+    compileOnly("maven.modrinth:corgilib:${minecraftVersion}-${project.properties["corgilib_version"]}-forge")
+    compileOnly("com.github.glitchfiend:TerraBlender-common:${minecraftVersion}-${project.properties["terrablender_version"]}")
+}
+
+tasks.register("optimise", OptimisePNGTask::class) {
+    option("i", 0)
+}
+
+/*
+publishing {
+    publications.create<MavenPublication>("mavenCommon") {
+        artifactId = "${project.properties["archives_base_name"]}" + "-Common"
+        from(components["java"])
+    }
+
+    repositories {
+        mavenLocal()
+        maven {
+            val releasesRepoUrl = "https://example.com/releases"
+            val snapshotsRepoUrl = "https://example.com/snapshots"
+            url = uri(if (project.version.toString().endsWith("SNAPSHOT") || project.version.toString().startsWith("0")) snapshotsRepoUrl else releasesRepoUrl)
+            name = "ExampleRepo"
+            credentials {
+                username = project.properties["repoLogin"]?.toString()
+                password = project.properties["repoPassword"]?.toString()
+            }
+        }
+    }
+}*/
---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+import net.fabricmc.loom.api.LoomGradleExtensionAPI
+
+plugins {
+    id("architectury-plugin") version "3.4-SNAPSHOT"
+    id("dev.architectury.loom") version "1.3-SNAPSHOT" apply false
+    id("com.matyrobbrt.mc.registrationutils") version "1.20.1-1.0.0"
+    idea
+    java
+}
+/*
+registrationUtils {
+    group("potionstudios.byg.reg")
+    println(subprojects)
+    projects {
+        "Common" {
+            type("common")
+            project(":Common")
+        }
+        "Fabric" {
+            type("fabric")
+            project(":Fabric")
+        }
+        "Forge" {
+            type("forge")
+            project(":Forge")
+        }
+    }
+}
+
+ */
+
+val minecraftVersion = project.properties["minecraft_version"] as String
+
+architectury.minecraft = minecraftVersion
+
+subprojects {
+    apply(plugin = "dev.architectury.loom")
+
+    val loom = project.extensions.getByName<LoomGradleExtensionAPI>("loom")
+
+    repositories {
+        mavenCentral()
+        mavenLocal()
+        maven("https://maven.parchmentmc.org")
+        maven("https://dl.cloudsmith.io/public/geckolib3/geckolib/maven/")
+        maven("https://api.modrinth.com/maven").content { includeGroup("maven.modrinth") }
+        maven("https://jitpack.io")
+    }
+
+    @Suppress("UnstableApiUsage")
+    dependencies {
+        "minecraft"("com.mojang:minecraft:$minecraftVersion")
+        "mappings"(loom.layered{
+            officialMojangMappings()
+            parchment("org.parchmentmc.data:parchment-$minecraftVersion:${project.properties["parchment"]}@zip")
+        })
+        compileOnly("com.google.auto.service:auto-service:1.1.1")
+        annotationProcessor("com.google.auto.service:auto-service:1.1.1")
+        compileOnly("org.jetbrains:annotations:24.0.1")
+    }
+    loom.silentMojangMappingsLicense()
+}
+
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "architectury-plugin")
+    apply(plugin = "maven-publish")
+    apply(plugin = "idea")
+
+    version = project.properties["version"] as String
+    group = project.properties["group"] as String
+    base.archivesName.set(project.properties["archives_base_name"] as String)
+
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(17)
+    }
+
+    java.withSourcesJar()
+}
